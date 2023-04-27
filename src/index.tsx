afcbc493392167863c97ccf3c6ca9b761c4eeda1
Omurbekova Tolkun <toprog27@gmail.com>
2023-04-27 20:33:09 +0600

    - changed wrong index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,19 @@
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 
-function Notes() {
-    const [newNote, setNewNote] = useState<string>("")
-    const [notes, setNotes] = useState<Array<string>>([])
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-        setNewNote(e.currentTarget.value)
-    const addNote = () => {
-        setNotes([newNote, ...notes])
-        setNewNote("")
-    }
-    return (
-        <div>
-            <textarea
-                value={newNote}
-                onChange={onChangeHandler}
-                onBlur={addNote}
-            />
-            <div>
-                <button
-                    onClick={()=>setNotes([])}
-                >Clear notes list</button>
-            </div>
-            <h4>Notes:</h4>
-            <div>
-                {notes.map(n => <p>{n}</p>)}
-            </div>
-        </div>
-    )
-}
 
-ReactDOM.render(
-    <Notes/>, document.getElementById('root')
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
-// Что надо написать вместо ххх,
-// чтобы при клике список заметок очищался?
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
-
-
-
-
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
